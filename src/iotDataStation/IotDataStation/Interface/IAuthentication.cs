97ac8bd8afb5e1b7dbd73242756865eab2cb6476
yccho <yccho@n3n.co.kr>
2018-11-13 09:34:47 +0900

    IAuthentication

---
+++ src/iotDataStation/IotDataStation/Interface/IAuthentication.cs
@@ -0,0 +1,10 @@
+﻿using Grapevine.Interfaces.Server;
+
+namespace IotDataStation.Interface
+{
+    public interface IAuthentication
+    {
+        bool ValidateUser(IHttpContext context);
+        string GetLoginPageUri(IHttpContext context);
+    }
+}
